e9127598447c130ba5eb3989d1d8f3f99a907953
y-tokushige <84838197+y-tokushige@users.noreply.github.com>
2021-07-28 13:13:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from streamlit import caching #キャッシュクリア
 from streamlit.script_runner import StopException, RerunException #rerun用
-st.set_page_config(page_title="AsdSystem-材料検査",layout = "wide") #画面を広く使うための設定
+st.set_page_config(page_title="AsdSystem-",layout = "wide") #画面を広く使うための設定
 import pandas as pd
 import numpy as np
 import sqlite3
 
+st.title("アサダメッシュ株式会社　来場者受付")
 st.text_input("テスト")

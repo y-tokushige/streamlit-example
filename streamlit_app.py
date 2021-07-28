89f94075639b80abc51d0f84af66069c968a016c
y-tokushige <84838197+y-tokushige@users.noreply.github.com>
2021-07-28 13:09:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from streamlit import caching #キャッシュクリア
+from streamlit.script_runner import StopException, RerunException #rerun用
+st.set_page_config(page_title="AsdSystem-材料検査",layout = "wide") #画面を広く使うための設定
+import pandas as pd
+import numpy as np
+import sqlite3
 
 st.text_input("テスト")

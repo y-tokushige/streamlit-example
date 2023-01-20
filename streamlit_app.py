af650ee6e52f8efaf4240a8fc4770c72b5907a1f
y-tokushige <84838197+y-tokushige@users.noreply.github.com>
2023-01-20 15:50:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@
 import streamlit as st
 st.set_page_config(page_title="AsdSystem-Excelリフレッシュ") #画面を広く使うための設定
 #import win32com.client
-import pythoncom
+#import pythoncom
 import tkinter
 from tkinter import filedialog
 import os

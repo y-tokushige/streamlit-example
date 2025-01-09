a93ae95e03621c58a49a5af072d21d4431de932e
y-tokushige <84838197+y-tokushige@users.noreply.github.com>
2025-01-09 10:58:45 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,20 @@
 import panel as pn
 
-# パネルの拡張機能を有効化
-pn.extension()
+# クリック時のイベント規定
+def click(event):
+    # ボタンのタイプがプライマリーだったら、表示を「押すなよ」にしてボタンタイプをサクセスに。
+    if button_e.button_type == "primary":
+        button_e.name = "押すなよ"
+        button_e.button_type = "success"
+    # ボタンのプライマリー以外だったら、表示を「押せよ」にしてボタンタイプをプライマリーに。
+    else:
+        button_e.name = "押せよ"
+        button_e.button_type = "primary"
 
-# テキスト入力ウィジェットを作成
-text_input = pn.widgets.TextInput(name='テキスト入力', placeholder='ここに入力してください...')
+# ボタンウィジェットの生成
+button_e = pn.widgets.Button(name= "押すとイベント実行")
+# clickした時のイベントを指定
+button_e.on_click(click)
 
-# 入力内容を表示する関数を定義
-def update_text(event):
-    text_output.value = text_input.value
-
-# テキスト入力の値が変更されたときに関数を呼び出す
-text_input.param.watch(update_text, 'value')
-
-# テキスト表示用のパネルを作成
-text_output = pn.pane.Markdown("入力された内容がここに表示されます")
-
-# レイアウトを作成
-layout = pn.Column(
-    "## シンプルなPanelサンプル",
-    text_input,
-    text_output
-)
-
-# アプリケーションとしてサーブ
-layout.servable()
+# 描画
+button_e
